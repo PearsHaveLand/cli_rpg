c6a760c52c45980e3cae2706940ff8e3599a9d26
pearce <pearce.haviland@gmail.com>
2017-05-06 18:26:46 -0400

    > Separated method definitions into header file > Made methods static > Removed time-based random seed due to uniformity of random numbers

--- Dice.cpp
+++ Dice.cpp
@@ -1,42 +1,43 @@
+#include "Dice.h"
 #include <cstdlib>
 
-int rollD2() {
+int Dice::rollD2() {
   //Random number between 1 and 2
   int random = (rand() % 2) + 1;
   return random;
 }
 
-int rollD4() {
+int Dice::rollD4() {
   //Random number between 1 and 4
   int random = (rand() % 4) + 1;
   return random;
 }
 
-int rollD6() {
+int Dice::rollD6() {
   //Random number between 1 and 6
   int random = (rand() % 6) + 1;
   return random;
 }
 
-int rollD8() {
+int Dice::rollD8() {
   //Random number between 1 and 2
   int random = (rand() % 8) + 1;
   return random;
 }
 
-int rollD10() {
+int Dice::rollD10() {
   //Random number between 1 and 2
   int random = (rand() % 10) + 1;
   return random;
 }
 
-int rollD12() {
+int Dice::rollD12() {
   //Random number between 1 and 2
   int random = (rand() % 12) + 1;
   return random;
 }
 
-int rollD20() {
+int Dice::rollD20() {
   //Random number between 1 and 2
   int random = (rand() % 20) + 1;
   return random;
