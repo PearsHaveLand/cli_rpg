4006c1a95a80326727532673a75718020ba0cd6c
pearce <pearce.haviland@gmail.com>
2017-05-06 18:29:58 -0400

    > Moved function definitions to separate header file > Made methods static

---
+++ Dice.h
@@ -0,0 +1,14 @@
+#ifndef DICE_H
+#define DICE_H
+
+class Dice {
+public:
+  static int rollD2();
+  static int rollD4();
+  static int rollD6();
+  static int rollD8();
+  static int rollD10();
+  static int rollD12();
+  static int rollD20();
+};
+#endif
