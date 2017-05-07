629fc5cdfb9134a63425cd6a1ec5a4c6e21cf2f3
pearce <pearce.haviland@gmail.com>
2017-05-07 02:01:08 -0400

    Created Monster class definition. Still need to implement and test

---
+++ Monster.h
@@ -0,0 +1,89 @@
+#ifndef MONSTER_H
+#define MONSTER_H
+
+#include "RPC.h"
+#include "Creature.h"
+
+class Monster : public Creature {
+
+  public:
+    Monster();
+
+  private:
+    //Experience points given on death
+    int expGiven;
+
+};
+#endif
+
+/*INHERITED FROM CREATURE**********************
+  //Getters
+  int getVitality(){return vitality;}
+  int getStrength(){return strength;}
+  int getDexterity(){return dexterity;}
+  int getWisdom(){return wisdom;}
+  int getIntelligence(){return intelligence;}
+  int getCharisma(){return charisma;}
+  int getPhysDefense(){return physDefense;}
+  int getMagDefense(){return magDefense;}
+  int getPhysAttack(){return physAttack;}
+  int getMagAttack(){return magAttack;}
+  int getAvoidability(){return avoidability;}
+  std::string getName(){return name;}
+
+
+  // makeAttack
+  // Takes pointer to RPC as argument
+  // Rolls d20 to see if hits
+  // Returns int, indicating the severity of the attack
+
+  int makeAttack(RPC*);
+
+  //Values added to actions based on
+  //	the DnD 5e formula of (stat - 10) / 2
+  int vitMod(){return (vitality - 10) / 2;}
+  int strMod(){return (strength - 10) / 2;}
+  int dexMod(){return (dexterity - 10) / 2;}
+  int wisMod(){return (wisdom - 10) / 2;}
+  int intMod(){return (intelligence - 10) / 2;}
+  int chaMod(){return (charisma - 10) / 2;}
+
+
+protected:
+  //Setters
+  void setVitality(int vit);
+  void setStrength(int str);
+  void setDexterity(int dex);
+  void setWisdom(int wis);
+  void setIntelligence(int intel);
+  void setCharisma(int cha);
+  void setPhysDefense(int def);
+  void setMagDefense(int def);
+  void setPhysAttack(int atk);
+  void setMagAttack(int atk);
+  void setAvoidability(int avo);
+  void setName(std::string nm);
+
+private:
+  //The main ability scores of the monster
+  int vitality;
+  int strength;
+  int dexterity;
+  int wisdom;
+  int intelligence;
+  int charisma;
+
+  //Hitpoints
+  int currenthp;
+  int totalhp;
+
+  //Dependent on equipment and ability scores
+  int physDefense;
+  int magDefense;
+  int physAttack;
+  int magAttack;
+  int avoidability;
+
+  std::string name;
+
+*/
